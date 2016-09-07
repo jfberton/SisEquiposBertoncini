be45ea6bda5090054dbfe846dc24e3db4779bbc8
José Federico Bertoncini <fede.berton@gmail.com>
2016-09-07 06:56:46 -0300

    Estilos de alert success y alert danger modificados

---
+++ SisEquiposBertoncini/Aplicativo/prueba_borrar.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SisEquiposBertoncini.Aplicativo
+{
+    public partial class prueba_borrar : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
